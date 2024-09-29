a2eb4d841d4731146a2e7f94f484485ce063e66d
Ruediger zu Dohna <snackbox@sinntr.eu>
2024-09-29 13:57:38 +0200

    add wiremock

--- src/main/java/com/github/t1/ExampleResource.java
+++ src/main/java/com/github/t1/ExampleResource.java
@@ -1,10 +1,19 @@
 package com.github.t1;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/hello")
 public class ExampleResource {
+    @RegisterRestClient(configKey = "name-service")
+    public interface NameServiceApi {
+        @GET @Path("/name") String name();
+    }
 
-    @GET public Greeting hello() {return new Greeting("Hello", "World");}
+    @Inject @RestClient NameServiceApi nameService;
+
+    @GET public Greeting hello() {return new Greeting("Hello", nameService.name());}
 }
--- src/test/java/com/github/t1/ExampleResourceTest.java
+++ src/test/java/com/github/t1/ExampleResourceTest.java
@@ -1,15 +1,31 @@
 package com.github.t1;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.quarkiverse.wiremock.devservice.ConnectWireMock;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@ConnectWireMock
 class ExampleResourceTest {
+    WireMock wireMock;
+
+    @BeforeEach void setup() {
+        wireMock.resetMappings();
+    }
+
     @Test
     void testHelloEndpoint() {
+        wireMock.register(get(urlEqualTo("/name"))
+                .willReturn(aResponse().withBody("World")));
+
         given()
                 .when().get("/hello")
                 .then()
